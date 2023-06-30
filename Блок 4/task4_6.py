2428790225077a2bbe85895698e5bef88a959eaf
pablodrev <smile.pashum@gmail.com>
2023-06-30 13:24:05 +0700

    Pt_4_6: Task completed
    Task 6 from part 4 completed

---
+++ Блок 4/task4_6.py
@@ -0,0 +1,12 @@
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet_rot13 = "nopqrstuvwxyzabcdefghijklm"
+d = {a:b for a, b in zip(alphabet, alphabet_rot13)}
+print(d)
+
+text = input("Enter some text to encode: ")
+
+new_text = ""
+for letter in text:
+    new_text += d[letter]
+
+print(new_text)
