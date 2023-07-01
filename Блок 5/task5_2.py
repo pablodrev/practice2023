a4571404040a918941910d64d8b9565691c7c9b5
pablodrev <smile.pashum@gmail.com>
2023-07-01 22:04:28 +0700

    Pt_5_2: Task completed
    Task 2 from part 5 completed

---
+++ Блок 5/task5_2.py
@@ -0,0 +1,37 @@
+import csv  # Библиотека для работы с .csv
+
+
+# Добавляет строку в .csv файл
+def csv_add_row(filename, row):
+    with open(filename, "a", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(row)
+
+
+filename = "prog_books.csv"
+
+# Добавление записей
+n_rows = int(input("Сколько записей вы ходите добавить?: "))
+for i in range(n_rows):
+    print(f"Добавление записи {i+1}")
+    book = input("Введите название книги: ")
+    author = input("Введите автора: ")
+    year = input("Введите год выпуска: ")
+    row = [book, author, year]
+    csv_add_row(filename, row)
+    print("Запись добавлена.\n")
+
+# Поиск книги по автору
+name = input("Введите автора для поиска его книг: ")
+with open(filename, "r", encoding="utf-8") as f:
+    reader = csv.DictReader(f)
+
+    # Для каждой строки в файле проверяем,
+    # содержит ли столбец "Автор" искомое значение
+    found = False
+    for row in reader:
+        if name in row["Автор"]:
+            found = True
+            print(row["Книга"])
+    if not found:
+        print("Книги не найдены.")
