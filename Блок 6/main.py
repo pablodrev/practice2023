ef8d98592f9c03408dd105671faa246c5daf5fd6
pablodrev <smile.pashum@gmail.com>
2023-07-02 22:04:10 +0700

    Comments added

--- Блок 6/main.py
+++ Блок 6/main.py
@@ -2,8 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 
+# Проверка на правильность ссылки
 while True:
     try:
+        # Получение HTML разметки
         url = input("Ссылка на исполнителя на Яндекс Музыке: ")
         response = requests.get(url + "/tracks")
         break
@@ -12,10 +14,12 @@ while True:
             requests.exceptions.InvalidSchema):
         print("Неверная ссылка! Попробуйте еще раз.")
 
+# Парсинг HTML
 soup = BeautifulSoup(response.text, "lxml")
 artist_name = soup.find("h1", class_="page-artist__title").text
 songs = soup.find_all("div", class_="d-track__name")
 
+# Вывод песен
 print(f"10 самых популярных треков исполнителя {artist_name}: ")
 for i in range(10):
     print(f"{i+1}. - {songs[i].text}")
