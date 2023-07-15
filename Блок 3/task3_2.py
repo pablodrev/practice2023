3f4be656a2593fef4d4b891fae7ae0069daecdff
pablodrev <smile.pashum@gmail.com>
2023-07-15 12:19:25 +0700

    PEP8 format

--- Блок 3/task3_2.py
+++ Блок 3/task3_2.py
@@ -2,5 +2,5 @@ from functools import reduce
 
 
 a = int(input("Введите число элементов: "))
-fib = lambda n: reduce(lambda x, _: x+[x[-1]+x[-2]], range(n-2), [0, 1])
+fib = lambda n: reduce(lambda x, _: x + [x[-1] + x[-2]], range(n - 2), [0, 1])
 print(fib(a))
--- Блок 3/task3_8.py
+++ Блок 3/task3_8.py
@@ -1,3 +1,3 @@
-alphabet = {letter: number+1 for number, letter in 
-            enumerate([chr(i) for i in range(ord("а"), ord("я")+1)])}
+alphabet = {letter: number + 1 for number, letter in 
+            enumerate([chr(i) for i in range(ord("а"), ord("я") + 1)])}
 print(alphabet)
